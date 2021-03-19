67969a4e593d153b2489dbe26f6184156113dbaa
陈雷 <903736662@qq.com>
2021-03-19 09:29:19 +0800

    mod: 关闭mock

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,7 +1,7 @@
 /// 全局静态变量存储地
 class Constant {
   /// 是否模拟数据
-  static const mock = true;
+  static const mock = false;
 
   /// 模拟的请求地址，开启了mock后，接口请求的地址不会从地址栏获取，而是用这个mock的地址
   static const mockHostName = "192.168.33.248";
